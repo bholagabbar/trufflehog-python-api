76a3824612e380d950c8d252f2242b921f172b3d
ShadyPastry <hwoodrow@andrew.cmu.edu>
2020-04-25 18:36:07 -0400

    Address pylint issues, defensively copy list parameters

--- api/name_pending/search_config.py
+++ api/name_pending/search_config.py
@@ -3,6 +3,9 @@ TODO: Documentation
 SearchConfig class.
 """
 
+from typing import List
+
+
 class SearchConfig():
   """
   A set of parameters that specify a search.
@@ -18,30 +21,52 @@ class SearchConfigBuilder():
   """
 
   def __init__(self):
-    pass
-  
-  def since_commmit(self, commit: int):
-    self._since_commit : int = commit
+    self._since_commit: int
+    self._max_depth: int
+    self._enable_entropy_checks: bool
+    self._branch: str
+    self._path_inclusions: List[str]
+
+  def since_commmit(self, commit: int) -> "SearchConfigBuilder":
+    """
+    TODO: Documentation
+    """
+    self._since_commit = commit
     return self
 
-  def max_depth(self, max_depth: int ):
-    self._max_depth : int = max_depth
+  def max_depth(self, max_depth: int) -> "SearchConfigBuilder":
+    """
+    TODO: Documentation
+    """
+    self._max_depth = max_depth
     return self
 
-  def enable_entropy_checks(self, enable_entropy: bool):
-    self._enable_entropy_checks: bool = enable_entropy
+  def enable_entropy_checks(self, enable_entropy: bool) -> "SearchConfigBuilder":
+    """
+    TODO: Documentation
+    """
+    self._enable_entropy_checks = enable_entropy
     return self
 
   def branch(self, branch: str):
-    self._branch: str = branch
+    """
+    TODO: Documentation
+    """
+    self._branch = branch
     return self
 
-  def path_inclusions(self, path_inclusions: str[]):
-    self._path_inclusions: str[] = path_inclusions
+  def path_inclusions(self, path_inclusions: List[str]) -> "SearchConfigBuilder":
+    """
+    TODO: Documentation
+    """
+    self._path_inclusions = list(path_inclusions)
     return self
 
-  def path_exclusions(self, path_exclusions: str[]):
-    self._path_inclusions: str[] = path_exclusions
+  def path_exclusions(self, path_exclusions: List[str]) -> "SearchConfigBuilder":
+    """
+    TODO: Documentation
+    """
+    self._path_inclusions: List[str] = list(path_exclusions)
     return self
 
   def build(self) -> SearchConfig:
--- api/name_pending/secret_finder.py
+++ api/name_pending/secret_finder.py
@@ -5,6 +5,7 @@ TODO: Documentation
 from typing import List
 from search_config import SearchConfig
 
+
 class Secret():
   """
   TODO: Documentation
@@ -17,29 +18,28 @@ class SecretFinder():
   TODO: Documentation
   """
 
-  # Maybe we don't need two methods for url vs. path, but I'm not sure the best way to distinguish these strings.
+  # Maybe we don't need two methods for url vs. path, but I'm not sure
+  # the best way to distinguish these strings.
   def find_secrets_with_url(self, git_url: str, search_config: SearchConfig) -> List[Secret]:
     """
     TODO: Documentation
     """
-    # TODO: Implement me
 
-  def find_secrets_with_repo_path(self, repo_path: str, search_config: SearchConfig) -> List[Secret]:
+  def find_secrets_with_repo_path(self,
+                                  repo_path: str,
+                                  search_config: SearchConfig) -> List[Secret]:
     """
     TODO: Documentation
     """
-    # TODO: Implement me
 
   # Parameters subject to change, may be cleaved into a few methods
   def find_secrets_with_regex(self, search_config: SearchConfig) -> List[Secret]:
     """
     TODO: Documentation
     """
-    # TODO: Implement me
 
   # As above, parameters subject to change, may be cleaved
   def find_batch_secrets(self, repo_path: str, search_config: SearchConfig) -> List[Secret]:
     """
     TODO: Documentation
     """
-    # TODO: Implement me
