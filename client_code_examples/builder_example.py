e69c8af980d191f1a719e0ec8adf7470d0436c94
jolshan <jolshan@andrew.cmu.edu>
2020-04-26 19:08:27 -0400

    Added some code to show a user trying a builder.

--- client_code_examples/builder_example.py
+++ client_code_examples/builder_example.py
@@ -10,7 +10,14 @@ def main():
   Creates and prints a builder.
   """
   builder = FindSecretsConfigBuilder()
+  builder.with_branch('dev').with_enable_entropy_checks(False).with_since_commit(150)
+
   print(builder)
+
+  print("Branch: %s" % builder.branch)
+
+  # Currently fails since there is no default
+  # print(builder.max_depth())
 
 if __name__ == '__main__':
   main()
