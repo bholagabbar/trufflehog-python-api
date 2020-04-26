d57607d78ca027281af8ad33cc36a1368a9d0c7c
ShadyPastry <hwoodrow@andrew.cmu.edu>
2020-04-26 16:29:33 -0400

    Fix main method in builder example

--- client_code_examples/builder_example.py
+++ client_code_examples/builder_example.py
@@ -5,6 +5,12 @@ Example of how to use the SearchConfigBuilder.
 from trufflehog_api import FindSecretsConfigBuilder
 
 
-def __main__():
+def main():
+  """
+  Creates and prints a builder.
+  """
   builder = FindSecretsConfigBuilder()
   print(builder)
+
+if __name__ == '__main__':
+  main()
