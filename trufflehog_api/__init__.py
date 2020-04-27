eae327b052a84dd5ae9e74455e7f4c5bab3e6283
ShadyPastry <hwoodrow@andrew.cmu.edu>
2020-04-26 20:55:51 -0400

    Add and implement find_secrets_remote and find_secrets_local
    - They simply create a Repo from the provided URL or path, then delegate to find_secrets
    
    Add documentation

--- trufflehog_api/__init__.py
+++ trufflehog_api/__init__.py
@@ -4,5 +4,6 @@ A rewritten version of the truffleHog API [1].
 [1]: https://github.com/dxa4481/truffleHog/tree/53a16df0a6f9a56ec11ad85abd193ea6feac5ff1
 """
 
+from .find_secrets import (Secret, find_secrets, find_secrets_local,
+                           find_secrets_remote)
 from .find_secrets_config import FindSecretsConfig, FindSecretsConfigBuilder
-from .find_secrets import Secret, find_secrets
