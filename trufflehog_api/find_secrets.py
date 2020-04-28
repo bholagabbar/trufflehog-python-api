56e15de7c556c333739016594438475fc5fc4b4a
jolshan <jolshan@andrew.cmu.edu>
2020-04-28 19:58:45 -0400

    Just trying out some ideas for the Secret object.

--- trufflehog_api/find_secrets.py
+++ trufflehog_api/find_secrets.py
@@ -2,6 +2,7 @@
 TODO: Documentation
 """
 
+import datetime
 import os
 import shutil
 import stat
@@ -19,7 +20,67 @@ class Secret():
   A secret found in a repository.
   """
 
-  def __init__(self):
+  def __init__(self,
+               commit_time: datetime.datetime,
+               branch_name: str,
+               prev_commit: str,
+               printable_diff: str,
+               commit_hash: str,
+               reason: str,
+               path: str):
+    """TODO"""
+    self._commit_time: datetime.datetime = commit_time
+    self._branch_name: str = branch_name
+    self._prev_commit: str = prev_commit
+    self._printable_diff: str = printable_diff
+    self._commit_hash: str = commit_hash
+    self._reason: str = reason
+    self._path: str = path
+
+  @property
+  def commit_time(self) -> datetime.datetime:
+    """TODO"""
+    return self._commit_time
+
+  @property
+  def branch_name(self) -> str:
+    """TODO"""
+    return self._branch_name
+
+  @property
+  def prev_commit(self) -> str:
+    """TODO"""
+    return self._prev_commit
+
+  @property
+  def printable_diff(self) -> str:
+    """TODO"""
+    return self._printable_diff
+
+  @property
+  def commit_hash(self) -> str:
+    """TODO"""
+    return self._commit_hash
+
+  @property
+  def reason(self) -> str:
+    """TODO"""
+    return self._reason
+
+  @property
+  def path(self) -> str:
+    """TODO"""
+    return self._path
+
+  def __str__(self):
+    """TODO"""
+    raise NotImplementedError()
+
+  def __repr__(self):
+    """TODO"""
+    raise NotImplementedError()
+
+  def to_dict(self):
     """TODO"""
     raise NotImplementedError()
 
