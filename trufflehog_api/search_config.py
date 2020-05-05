7f22d68d4d77cf2caf66e8998cca4903df65f05b
Shreyans Sheth <dev.shreyans96@gmail.com>
2020-05-04 22:00:48 -0400

    Fix docstring lengths

--- trufflehog_api/search_config.py
+++ trufflehog_api/search_config.py
@@ -15,29 +15,33 @@ class SearchConfig:
                  entropy_checks_enabled: bool = True,
                  regexes: Dict[str, str] = None,
                  ):
-        """Creates a new default search configuration object with entropy and regex checks switched off
+        """Creates a new default search configuration object with entropy and regex checks off
 
         :param str max_depth:
             Maximum commit depth to continue search till (default is 1000000)
         :param list include_search_paths:
             List of regular expressions restricting search to matching object paths only. At least
             one of these must match a Git object path in order for the repository to be searched
-            (default is None, all object paths are searched unless otherwise excluded by search_paths_excluded)
+            (default is None, all object paths are searched unless otherwise excluded by
+            search_paths_excluded)
         :param list exclude_search_paths:
             List of regular expressions restricting search to exclude matching object paths
             (default is None, no object paths are excluded).
         :param str entropy_checks_enabled:
-            Enable high signal entropy checks, this is the default secret finding mechanism of the library.
-            truffleHog will evaluate the shannon entropy for both the base64 char set and hexadecimal char set
-            for every blob of text greater than 20 characters comprised  of those character sets in each diff.
-            If at any point a high entropy string >20 characters is detected
-            (default is True, this is the default secret finding mechanism in the library).
+            Enable high signal entropy checks, this is the default secret finding mechanism of the
+            library. truffleHog will evaluate the shannon entropy for both the base64 char set and
+            hexadecimal char set for every blob of text greater than 20 characters comprised  of
+            those character sets in each diff. If at any point a high entropy string >20 characters
+            is detected
+            (default is True, this is the default secret finding mechanism).
         :param dict regexes:
-            Use this argument to pass in a custom dictionary of regexes, eg. to search for project specific
-            strings. The dictionary has keys as the description of a regex and value as the the regex string
-            itself. For the user's convenience, the library provides a prebuilt regex dict populated with
-            regexes corresponding to popular 3rd party API services. This dict is accessible as a static method
-            SearchConfig.default_regexes(). Search may be slower than than usual (default is None, no regexes to search)
+            Use this argument to pass in a custom dictionary of regexes, eg. to search for project
+            specific strings. The dictionary has keys as the description of a regex and value as
+            the the regex string itself. For the user's convenience, the library provides a
+            prebuilt regex dict populated with regexes corresponding to popular 3rd party API
+            services. This dict is accessible as a static method SearchConfig.default_regexes().
+            Search may be slower than than usual
+            (default is None, no regexes to search)
         """
 
         self._max_depth: int = max_depth
@@ -50,32 +54,49 @@ class SearchConfig:
 
     @property
     def max_depth(self) -> int:
-        """:return: Returns an integer corresponding to the maximum commit depth to continue search till."""
+        """
+        :return: Returns an integer which is the the maximum commit depth to continue
+        search till
+        """
         return self._max_depth
 
     @property
     def include_search_paths(self) -> List[str]:
-        """:return: Returns a copy of the list of regexes restricting search to matching object paths only."""
+        """
+        :return: Returns a copy of the list of regexes restricting search to matching
+        object paths only
+        """
         return copy(self._include_search_paths)
 
     @property
     def exclude_search_paths(self) -> List[str]:
-        """:return: Returns a copy of the list of regexes restricting search to exclude matching object paths."""
+        """
+        :return: Returns a copy of the list of regexes restricting search to exclude
+        matching object paths.
+        """
         return copy(self._exclude_search_paths)
 
     @property
     def entropy_checks_enabled(self) -> bool:
-        """:return: Returns a boolean value indicating whether entropy checks are enabled"""
+        """
+        :return: Returns a boolean value indicating whether entropy checks are enabled
+        """
         return self._entropy_checks_enabled
 
     @property
     def regexes(self) -> Dict[str, str]:
-        """:return: Returns a copy of the dict of custom regexes to search search matching project strings against"""
+        """
+        :return: Returns a copy of the dict of custom regexes to search search matching
+        project strings against
+        """
         return copy(self._regexes)
 
     @staticmethod
     def default_regexes() -> Dict[str, str]:
-        """:return: Returns a copy of the prebuilt regex dict provided by truffleHogRegexes"""
+        """
+        :return: Returns a copy of the prebuilt regex dict provided by truffleHogRegexes
+        library
+        """
         return copy(default_regexes)
 
     def __str__(self):
